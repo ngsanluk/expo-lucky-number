492d65f08bd46264ed39c7a6c744abf3264766f4
Sunny NG <ngsanluk@gmail.com>
2021-10-26 23:45:56 +0800

    changes App.js

--- App.js
+++ App.js
@@ -1,14 +1,49 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default class App extends React.Component {
+
+  state = {
+    luckyNumber: 0
+  }
+
+  play = () => {
+    var luckyNumber = Math.floor((Math.random() * 100) + 1);
+    this.setState({
+      luckyNumber: luckyNumber
+    })
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text
+          style={{
+            color: "#333333",
+            fontSize: 20,
+            paddingTop: 100,
+            paddingBottom: 20
+          }}>
+          Lucky Number (expo)
+        </Text>
+        <Text
+          style={{
+            color: "#CC0000",
+            fontSize: 200,
+            paddingTop: 100,
+            paddingBottom: 100
+          }}>
+          {this.state.luckyNumber==0 ? '?' : this.state.luckyNumber}
+        </Text>
+        <Button
+          title="PLAY"
+          onPress={this.play}
+        />
+      </View>
+    );
+  }
+
 }
 
 const styles = StyleSheet.create({
